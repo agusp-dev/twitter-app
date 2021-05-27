f42aa165689df2ff959cc82614f3ab07cc0acd77
agusp-dev <perezp.agustin@gmail.com>
2021-05-27 13:01:56 -0300

    Implement styles export to another file.

--- components/appLayout/AppLayout.js
+++ components/appLayout/AppLayout.js
@@ -1,51 +1,13 @@
-import { fonts, colors, breakpoints } from '../../styles/theme'
-import { addOpacityToColor } from '../../styles/utils'
+import styles, { globalStyles } from './styles'
 
 export default function AppLayout({ children }) {
-	const backgroundColor = addOpacityToColor(colors.primary, 0.3)
   return (
 		<>
 			<div>
         <main>{ children }</main>
       </div>
-      <style jsx>{`
-        div {
-          height: 100vh;
-          display: grid;
-          place-items: center;
-        }
-        main {
-          width: 100%;
-          height: 100%;
-          background: ${colors.white};
-          box-shadow: 0 10px 25px rgba(0, 0, 0, .3);
-          border-radius: 10px;
-        }
-
-        @media (min-width: ${breakpoints.mobile}) {
-          main {
-            width: ${breakpoints.mobile};
-            height: 90vh;
-          }
-        }
-
-      `}</style>
-			<style jsx global>{`
-				html,
-				body {
-					background-image: 
-						radial-gradient(${backgroundColor} 1px, transparent 1px),
-						radial-gradient(${backgroundColor} 1px, transparent 1px);
-					background-position: 0 0 25px 25px;
-					background-size: 50px 50px;
-					padding: 0;
-					margin: 0;
-					font-family: ${fonts.base}
-				}
-				* {
-					box-sizing: border-box;
-				}
-		`}</style>
+      <style jsx>{ styles }</style>
+			<style jsx global>{ globalStyles }</style>
 		</>
 	)
 }
