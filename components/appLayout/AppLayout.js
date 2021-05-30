2fd5cc86887ee15478262b2db81d6e227523980a
agusp-dev <perezp.agustin@gmail.com>
2021-05-29 21:36:31 -0300

    Integrated husky

--- components/appLayout/AppLayout.js
+++ components/appLayout/AppLayout.js
@@ -1,18 +1,20 @@
-import PropTypes from 'prop-types'
-import styles, { globalStyles } from './styles'
+import PropTypes from "prop-types"
+import styles, { globalStyles } from "./styles"
 
-export default function AppLayout ({ children }) {
+export default function AppLayout({ children }) {
   return (
     <>
       <div>
-        <main>{ children }</main>
+        <main>{children}</main>
       </div>
-      <style jsx>{ styles }</style>
-      <style jsx global>{ globalStyles }</style>
+      <style jsx>{styles}</style>
+      <style jsx global>
+        {globalStyles}
+      </style>
     </>
   )
 }
 
 AppLayout.propTypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
 }
